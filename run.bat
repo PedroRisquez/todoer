7c0abd58f0dd344062c2dd1d5e00ab5e62ae85bc
Pedro Rísquez Calvillo <pedrorisquez@hotmail.com>
2024-06-05 09:42:33 +0200

    comit msg

---
+++ run.bat
@@ -0,0 +1,8 @@
+set FLASK_APP=todo
+set FLASK_ENV=development
+set FLASK_DATABASE_HOST=localhost
+set FLASK_DATABASE_USER=root
+set FLASK_DATABASE_PASSWORD=
+set FLASK_DATABASE=prueba
+@REM flask init-db
+flask run
